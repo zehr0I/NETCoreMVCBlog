c0a532c1de42d5b894c9e872a01fed61ff0c641d
zehra <zehra@DESKTOP-C9FLMVJ>
2023-07-13 16:04:12 +0300

    yeni özellikProje dosyası ekle.

---
+++ Models/Yorum.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace NETCoreMVCBlog.Models
+{
+    public class Yorum 
+    {
+        public string Yorumcu_Adi{ get; set; }
+        public string Yorum_Konusu { get; set; }
+        public bool Yorum_Durumu { get; set; }//iyi veya kötü yorum değerlendirme.
+        public DateTime Yorum_Tarihi{ get; set; }
+    }
+}
