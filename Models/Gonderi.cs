d13b7c0a005be633762d4c3fbdd5709802412497
zehra <zehra@DESKTOP-C9FLMVJ>
2023-07-13 16:04:12 +0300

    Proje dosyası ekle.

---
+++ Models/Gonderi.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace NETCoreMVCBlog.Models
+{
+    public class Gonderi 
+    {
+        public DateTime Gonderi_Zamani { get; set; }
+        public string Gonderi_Baslik { get; set; }
+        public string Gonderi_Icerik{ get; set; }
+    }
+}
